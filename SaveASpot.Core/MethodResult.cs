98c41d2c1d64c784cce2da4cef616f8aedc04dfc
Ivan_Dziameshka <Ivan_Dziameshka@epam.com>
2013-07-09 17:26:21 +0300

    MethodResult added. AccountControllerService added.

---
+++ SaveASpot.Core/MethodResult.cs
@@ -0,0 +1,17 @@
+﻿namespace SaveASpot.Core
+{
+	public sealed class MethodResult<T> : IMethodResult<T>
+	{
+		private readonly bool _isSuccess;
+		private readonly T _status;
+
+		public MethodResult(bool isSuccess, T status)
+		{
+			_isSuccess = isSuccess;
+			_status = status;
+		}
+
+		public bool IsSuccess { get { return _isSuccess; } }
+		public T Status { get { return _status; } }
+	}
+}
---
+++ Tests/TestsSaveASpot/Core/MethodResultTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using SaveASpot.Core;
+
+namespace TestsSaveASpot.Core
+{
+	[TestFixture]
+	public sealed class MethodResultTests
+	{
+		public static object[] CtorTestCasesSource = new object[]
+		{
+			new object[]{true, new Status()},
+			new object[]{false, new Status()},
+		};
+
+		[Test]
+		[TestCaseSource("CtorTestCasesSource")]
+		public void ctor_should_initialize_properties(bool isSuccess, Status expected)
+		{
+			//arrange
+			//act
+			var target = new MethodResult<Status>(isSuccess, expected);
+			//assert
+			Assert.AreEqual(isSuccess, target.IsSuccess);
+			Assert.AreEqual(expected, target.Status);
+		}
+
+		public sealed class Status { }
+	}
+}
