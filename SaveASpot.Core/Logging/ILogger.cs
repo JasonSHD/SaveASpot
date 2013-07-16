909815ad6c11fd96d2ce0bdea8cabe9c7a3a6817
Eugeniy Dubovsky <dubovsky.e@gmail.com>
2013-07-16 12:44:11 +0400

    Refactoring for Logger

--- SaveASpot.Core/Logging/ILogger.cs
+++
@@ -1,8 +0,0 @@
-﻿
-namespace SaveASpot.Core.Logging
-{
-	public interface ILogger
-	{
-		void Log(ILogEntry logEntry);
-	}
-}
--- SaveASpot.Repositories/Interfaces/Logging/ILogAppender.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace SaveASpot.Repositories.Interfaces.Logging
-{
-	public interface ILogAppender
-	{
-		void Log(string level, string message, Exception exception);
-	}
-}
