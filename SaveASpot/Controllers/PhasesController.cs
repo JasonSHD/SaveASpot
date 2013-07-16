8ff9d06f8fec24821999ae6cf1eb2328b04efa92
Roman Savenko <savenrom@gmail.com>
2013-07-17 00:41:03 +0300

    phases and parsels page added

--- SaveASpot/Controllers/PhasesController.cs
+++ SaveASpot/Controllers/PhasesController.cs
@@ -1,8 +1,13 @@
-﻿using System.Web.Mvc;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
 using SaveASpot.Controllers.Artifacts;
 using SaveASpot.Core.Web.Mvc;
+using SaveASpot.ViewModels;
 
 namespace SaveASpot.Controllers
+
 {
 	[TabDescriptions(SiteConstants.PhasesControllerAlias, "Phases")]
 	[AdministratorAuthorize]
@@ -12,5 +17,35 @@ namespace SaveASpot.Controllers
 		{
 			return TabView(new { });
 		}
+
+        [HttpPost]
+        public ActionResult UploadPhases(IEnumerable<HttpPostedFileBase> postedPhases)
+        {
+            foreach (var file in postedPhases)
+            {
+                if (file != null && file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                    //file.SaveAs(path);
+                }
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult UploadParcels(IEnumerable<HttpPostedFileBase> postedParcels)
+        {
+            foreach (var file in postedParcels)
+            {
+                if (file != null && file.ContentLength > 0)
+                {
+                    var fileName = Path.GetFileName(file.FileName);
+                    var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
+                    //file.SaveAs(path);
+                }
+            }
+            return RedirectToAction("Index");
+        }
 	}
 }
