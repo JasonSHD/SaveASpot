a9edd607a0a7b149961e7b55e9824ef24b5ffec8
Roman Savenko <savenrom@gmail.com>
2013-07-18 00:18:44 +0300

    spots and parcels upload done

--- SaveASpot/Controllers/PhasesController.cs
+++ SaveASpot/Controllers/PhasesController.cs
@@ -1,33 +1,68 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 using SaveASpot.Controllers.Artifacts;
+using SaveASpot.Core.Logging;
 using SaveASpot.Core.Web.Mvc;
-using SaveASpot.ViewModels;
+using SaveASpot.Services.Interfaces;
 
 namespace SaveASpot.Controllers
-
 {
-	[TabDescriptions(SiteConstants.PhasesControllerAlias, "Phases")]
-	[AdministratorAuthorize]
-	public sealed class PhasesController : AdminTabController
-	{
-		public ActionResult Index()
-		{
-			return TabView(new { });
-		}
+    [TabDescriptions(SiteConstants.PhasesControllerAlias, "Phases")]
+    [AdministratorAuthorize]
+    public sealed class PhasesController : AdminTabController
+    {
+        private readonly IArcgisService _arcgisService;
+        private readonly ILogger _logger;
+
+        public ActionResult Index()
+        {
+            return TabView(new { });
+        }
+
+        public PhasesController(IArcgisService arcgisService, ILogger logger)
+        {
+            try
+            {
+                if (ReferenceEquals(null, arcgisService)) throw new ArgumentNullException("IArcgisService is null");
+                if (ReferenceEquals(null, logger)) throw new ArgumentNullException("ILogger is null");
+
+                _logger = logger;
+                _arcgisService = arcgisService;
+            }
+            catch (Exception ex)
+            {
+                if (_logger != null)
+                {
+                    _logger.Error(ex.Message, ex);
+                }
+                else throw;
+            }
+        }
 
         [HttpPost]
-        public ActionResult UploadPhases(IEnumerable<HttpPostedFileBase> postedPhases)
+        public ActionResult UploadSpots(IEnumerable<HttpPostedFileBase> postedSpots)
         {
-            foreach (var file in postedPhases)
+            foreach (var file in postedSpots)
             {
                 if (file != null && file.ContentLength > 0)
                 {
-                    var fileName = Path.GetFileName(file.FileName);
-                    var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
-                    //file.SaveAs(path);
+                    try
+                    {
+                        BinaryReader b = new BinaryReader(file.InputStream);
+                        byte[] binData = b.ReadBytes((int)file.InputStream.Length);
+                        string spotsResult = System.Text.Encoding.UTF8.GetString(binData);
+                        _arcgisService.AddSpots(spotsResult);
+                    }
+                    catch (Exception ex)
+                    {
+                        if (_logger == null) throw;
+                        _logger.Error(string.Format("Class: {0}; Method: {1}; ex.message:{2}", this.GetType().FullName, MethodBase.GetCurrentMethod().Name, ex.Message), ex);    
+                    }
+                  
                 }
             }
             return RedirectToAction("Index");
@@ -40,12 +75,22 @@ namespace SaveASpot.Controllers
             {
                 if (file != null && file.ContentLength > 0)
                 {
-                    var fileName = Path.GetFileName(file.FileName);
-                    var path = Path.Combine(Server.MapPath("~/App_Data/uploads"), fileName);
-                    //file.SaveAs(path);
+                    try
+                    {
+                        BinaryReader b = new BinaryReader(file.InputStream);
+                        byte[] binData = b.ReadBytes((int)file.InputStream.Length);
+                        string parcelsResult = System.Text.Encoding.UTF8.GetString(binData);
+                        _arcgisService.AddParcels(parcelsResult);
+                    }
+                    catch (Exception ex)
+                    {
+                        if (_logger == null) throw;
+                        _logger.Error(string.Format("Class: {0}; Method: {1}; ex.message:{2}", this.GetType().FullName, MethodBase.GetCurrentMethod().Name, ex.Message), ex);
+                    }
+                   
                 }
             }
             return RedirectToAction("Index");
         }
-	}
+    }
 }
