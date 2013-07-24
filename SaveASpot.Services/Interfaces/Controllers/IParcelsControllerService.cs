2887f427cc6b223c30ce68cdf463917d8f940c1f
Ivan_Dziameshka <Ivan_Dziameshka@epam.com>
2013-07-24 19:59:02 +0300

    Parcels display implemented.

--- SaveASpot.Services/Interfaces/Controllers/IParcelsControllerService.cs
+++ SaveASpot.Services/Interfaces/Controllers/IParcelsControllerService.cs
@@ -6,6 +6,7 @@ namespace SaveASpot.Services.Interfaces.Controllers
 	public interface IParcelsControllerService
 	{
 		ParcelsViewModel GetParcels(SelectorViewModel selectorViewModel);
+		ParcelsViewModel ByPhase(string identity);
 		IMethodResult Remove(string identity);
 	}
 }
