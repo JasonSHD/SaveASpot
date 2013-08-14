d04692b23034dfc6e51139561b108bbbc75f137f
Ivan_Dziameshka <Ivan_Dziameshka@epam.com>
2013-08-14 16:47:39 +0300

    Remove unused classes

--- SaveASpot.Core/IValidator.cs
+++ SaveASpot.Core/IValidator.cs
@@ -1,20 +1,5 @@
-using SaveASpot.Core.Validation;
-
 namespace SaveASpot.Core
 {
-	public interface IValidator<in T>
-	{
-		IValidationResult Validate(T input);
-	}
-
-	public static class ValidatorExtensions
-	{
-		public static IValidator<T> And<T>(this IValidator<T> validator, IValidator<T> and)
-		{
-			return new AndValidator<T>(validator, and);
-		}
-	}
-
 	public interface IValidator
 	{
 		IValidationResult Validate(object input);
--- SaveASpot.Core/Validation/AndValidator.cs
+++ SaveASpot.Core/Validation/AndValidator.cs
@@ -1,27 +1,5 @@
 namespace SaveASpot.Core.Validation
 {
-	public sealed class AndValidator<T> : IValidator<T>
-	{
-		private readonly IValidator<T> _left;
-		private readonly IValidator<T> _right;
-
-		public AndValidator(IValidator<T> left, IValidator<T> right)
-		{
-			_left = left;
-			_right = right;
-		}
-
-		public IValidationResult Validate(T input)
-		{
-			var validationResult = _left.Validate(input);
-			if (!validationResult.IsValid) return validationResult;
-			validationResult = _right.Validate(input);
-			if (!validationResult.IsValid) return validationResult;
-
-			return validationResult;
-		}
-	}
-
 	public sealed class AndValidator : IValidator
 	{
 		private readonly IValidator _left;
