00e9a33ae2404dcc8d19059d39ee939791940f16
Ivan_Dziameshka <Ivan_Dziameshka@epam.com>
2013-09-25 19:33:46 +0300

    Defaul settings moved from jmaag 6cdd0c8c99745061ccae264d56ffbd25f1906aa4 change

--- SaveASpot/Scripts/js/saveaspot.site.mapTab.js
+++ SaveASpot/Scripts/js/saveaspot.site.mapTab.js
@@ -5,13 +5,11 @@
 	var gmap;
 	q.controls.gmap(gmapKey, function () {
 		var mapOptions = {
-			zoom: 8,
-			// ReSharper disable UseOfImplicitGlobalInFunctionScope
-			center: new google.maps.LatLng(0, 0),
-			mapTypeId: google.maps.MapTypeId.ROADMAP
+			zoom: 17,
+			center: new window.google.maps.LatLng(40.58822748562923, -111.58563494682312),
+			mapTypeId: window.google.maps.MapTypeId.ROADMAP
 		};
 		gmap = new google.maps.Map(gmapCanvas, mapOptions);
-		// ReSharper restore UseOfImplicitGlobalInFunctionScope
 	});
 
 	var controlsForDestroy = [];
